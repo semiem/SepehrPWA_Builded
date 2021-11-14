a1049ef753b81f487a468333414c6fdce55f6fbf
Milad Emadi <milademadi74@gmail.com>
2021-11-14 11:09:52 +0330

    ...

---
+++ uploads/files/sepehrIRIB/android/api/common/banners.php
@@ -0,0 +1,81 @@
+﻿{
+  "homeStructureRows": [
+    {
+      "rowType": "STRUCT_TYPE_BANNER",
+      "rowData": {
+        "src": "https://cdn1.shimaiptv.ir/uploads/app/shimaott/android/liveIRIB/android/common/images/programs/sports.png?t=1",
+        "title": "شبکه های ورزشی",
+        "onClick": {
+          "method": "OPEN_PAGE_CHANNELS",
+          "field": "type=tv;channelId=3;channelId=117;channelId=17"
+        }
+      }
+    },
+    {
+      "rowType": "STRUCT_TYPE_BANNER",
+      "rowData": {
+        "src": "https://cdn1.shimaiptv.ir/uploads/app/shimaott/android/liveIRIB/android/common/images/programs/news.png?t=1",
+        "title": "شبکه های خبری",
+        "onClick": {
+          "method": "OPEN_PAGE_CHANNELS",
+          "field": "type=tv;channelId=6;channelId=15;channelId=123;channelId=119"
+        }
+      }
+    },
+    {
+      "rowType": "STRUCT_TYPE_BANNER",
+      "rowData": {
+        "src": "https://cdn1.shimaiptv.ir/uploads/app/shimaott/android/liveIRIB/android/common/images/programs/kids.png?t=1",
+        "title": "شبکه های کودک و نوجوان",
+        "onClick": {
+          "method": "OPEN_PAGE_CHANNELS",
+          "field": "channelId=109;channelId=20;channelId=116"
+        }
+      }
+    },
+    {
+      "rowType": "STRUCT_TYPE_BANNER",
+      "rowData": {
+        "src": "https://cdn1.shimaiptv.ir/uploads/app/shimaott/android/liveIRIB/android/common/images/programs/film.png?t=1",
+        "title": "فیلم و سریال",
+        "onClick": {
+          "method": "OPEN_PAGE_CHANNELS",
+          "field": "channelId=12;channelId=75;channelId=114;channelId=14"
+        }
+      }
+    },
+    {
+      "rowType": "STRUCT_TYPE_BANNER",
+      "rowData": {
+        "src": "https://cdn1.shimaiptv.ir/uploads/app/shimaott/android/liveIRIB/android/common/images/programs/documentary.png?t=1",
+        "title": "مستند",
+        "onClick": {
+          "method": "OPEN_PAGE_CHANNELS",
+          "field": "channelId=9"
+        }
+      }
+    },
+    {
+      "rowType": "STRUCT_TYPE_BANNER",
+      "rowData": {
+        "src": "https://cdn1.shimaiptv.ir/uploads/app/shimaott/android/liveIRIB/android/common/images/programs/religious.png?t=1",
+        "title": "مذهبی",
+        "onClick": {
+          "method": "OPEN_PAGE_CHANNELS",
+          "field": "channelId=8;channelId=68;"
+        }
+      }
+    },
+	{
+      "rowType": "STRUCT_TYPE_BANNER",
+      "rowData": {
+        "title": "امروز با من",
+        "src": "https://cdn1.shimaiptv.ir/uploads/app/shimaott/android/liveIRIB/android/common/images/slideshow/emrooz_ba_man.jpg",
+        "onClick": {
+          "method": "",
+          "field": "<a href='https://sepehr.irib.ir/emrooz_ba_man/'>........</a>"
+        }
+      }
+    }
+  ]
+}
---
+++ uploads/files/sepehrIRIB/android/api/v2.3/main.php
@@ -0,0 +1,300 @@
+﻿{
+  "homeStructureRows": [
+    {
+      "rowType": "STRUCT_TYPE_TAG",
+      "rowData": {
+        "data": [
+          {
+            "title": "جدول پخش",
+            "onClick": {
+              "method": "OPEN_EPG_TABLE",
+              "field": ""
+            }
+          },
+          {
+            "title": "علاقه مندی ها",
+            "onClick": {
+              "method": "OPEN_PAGE_FAVORITE_CHANNELS",
+              "field": ""
+            }
+          },
+          {
+            "title": "ویدیو های مورد علاقه",
+            "onClick": {
+              "method": "OPEN_PAGE_FAVORITE_VODS",
+              "field": ""
+            }
+          },
+          {
+            "title": "در حال پخش",
+            "onClick": {
+              "method": "OPEN_PAGE_PLAYING_PROGRAMS",
+              "field": ""
+            }
+          },
+          {
+            "title": "دسته بندی پخش زنده",
+            "onClick": {
+              "method": "OPEN_PAGE_LIVE_CATS",
+              "field": "1"
+            }
+          },
+          {
+            "title": "دسته بندی ویدیو",
+            "onClick": {
+              "method": "OPEN_PAGE_VOD_CATS",
+              "field": "5"
+            }
+          },
+          {
+            "title": "فیلم های اختصاصی سپهر",
+            "onClick": {
+              "method": "OPEN_PAGE_VOD_LIST",
+              "field": "66"
+            }
+          }
+        ],
+        "type": "TYPE_TAG_DEFAULT"
+      }
+    },
+    {
+      "rowType": "STRUCT_TYPE_NEWLINE_8"
+    },
+    {
+      "rowType": "STRUCT_TYPE_SLIDE_SHOW",
+      "rowData": {
+        "data": [
+          {
+            "src": "https://sepehr.irib.ir/uploads/files/sepehrIRIB/android/common/images/slideshow/irseries.jpg",
+            "title": "سریال های ایرانی",
+            "onClick": {
+              "method": "OPEN_PAGE_VOD_CATS",
+              "field": "111"
+            }
+          },
+          {
+            "src": "https://sepehr.irib.ir/uploads/files/sepehrIRIB/android/common/images/slideshow/besharat.jpg",
+            "title": "بشارت",
+            "onClick": {
+              "method": "OPEN_PAGE_VOD_LIST",
+              "field": "1096"
+            }
+          },
+		  {
+            "src": "https://sepehr.irib.ir/uploads/files/sepehrIRIB/android/common/images/slideshow/caffefilm.jpg",
+            "title": "کافه فیلم",
+            "onClick": {
+              "method": "OPEN_PAGE_VOD_LIST",
+              "field": "1099"
+            }
+          }
+        ]
+      }
+    },
+    {
+      "rowType": "STRUCT_TYPE_LINE"
+    },
+    {
+      "rowType": "STRUCT_TYPE_HALF_BANNER",
+      "rowData": {
+        "data": [
+          {
+            "src": "https://cdn1.shimaiptv.ir/uploads/app/shimaott/android/liveIRIB/android/common/files/saarasari.png?t=1",
+            "title": "سراسری",
+            "onClick": {
+              "method": "OPEN_PAGE_CHANNEL_LIST",
+              "field": "2"
+            }
+          },
+          {
+            "src": "https://cdn1.shimaiptv.ir/uploads/app/shimaott/android/liveIRIB/android/common/files/ostani.png?t=1",
+            "title": "استانها",
+            "onClick": {
+              "method": "OPEN_PAGE_CHANNEL_LIST",
+              "field": "3"
+            }
+          },
+          {
+            "src": "https://cdn1.shimaiptv.ir/uploads/app/shimaott/android/liveIRIB/android/common/files/boronmarzi.png?t=1",
+            "title": "برون مرزی",
+            "onClick": {
+              "method": "OPEN_PAGE_CHANNEL_LIST",
+              "field": "4"
+            }
+          }
+        ],
+        "type": "TYPE_HALF_BANNER_THIN"
+      }
+    },
+    {
+      "rowType": "STRUCT_TYPE_LINE"
+    },
+    {
+      "rowType": "STRUCT_TYPE_ROW_HORIZONTAL_ITEM",
+      "rowData": {
+        "title": "همه کانال های سیما",
+        "item": {
+          "data": null,
+          "type": "TYPE_PLAYING_PROGRAM_HORIZONTAL",
+          "onLoad": {
+            "method": "TYPE_SHOW_CHANNELS",
+            "field": "2"
+          }
+        }
+      }
+    },
+    {
+      "rowType": "STRUCT_TYPE_ROW_HORIZONTAL_ITEM",
+      "rowData": {
+        "title": "شبکه های اختصاصی سپهر",
+        "item": {
+          "data": null,
+          "type": "TYPE_LIVE_TV_GRID",
+          "onLoad": {
+            "method": "TYPE_SHOW_CHANNELS",
+            "field": "1080"
+          }
+        }
+      }
+    },
+    {
+      "rowType": "STRUCT_TYPE_LINE"
+    },
+    {
+      "rowType": "STRUCT_TYPE_BANNER",
+      "rowData": {
+        "title": "لیست شبکه ها",
+        "src": "https://sepehr.irib.ir/uploads/files/sepehrIRIB/android/common/images/v3/live.jpg",
+        "onClick": {
+          "method": "OPEN_PAGE_LIVE_CATS",
+          "field": "1"
+        }
+      }
+    },
+    {
+      "rowType": "STRUCT_TYPE_NEWLINE_8"
+    },
+    {
+      "rowType": "STRUCT_TYPE_BANNER",
+      "rowData": {
+        "title": "لیست محتواهای درخواستی",
+        "src": "https://sepehr.irib.ir/uploads/files/sepehrIRIB/android/common/images/v3/vod.jpg",
+        "onClick": {
+          "method": "OPEN_PAGE_VOD_CATS",
+          "field": "5"
+        }
+      }
+    },
+    {
+      "rowType": "STRUCT_TYPE_NEWLINE_8"
+    },
+    {
+      "rowType": "STRUCT_TYPE_HALF_BANNER",
+      "rowData": {
+        "data": [
+          {
+            "src": "https://sepehr.irib.ir/uploads/files/sepehrIRIB/android/common/images/v3/fav_live.jpg",
+            "title": "شبکه های مورد علاقه",
+            "onClick": {
+              "method": "OPEN_PAGE_FAVORITE_CHANNELS",
+              "field": ""
+            }
+          },
+          {
+            "src": "https://sepehr.irib.ir/uploads/files/sepehrIRIB/android/common/images/v3/fav_vod.jpg",
+            "title": "ویدیوهای مورد علاقه",
+            "onClick": {
+              "method": "OPEN_PAGE_FAVORITE_VODS",
+              "field": ""
+            }
+          },
+          {
+            "src": "https://sepehr.irib.ir/uploads/files/sepehrIRIB/android/common/images/v3/epg.jpg",
+            "title": "جدول پخش",
+            "onClick": {
+              "method": "OPEN_EPG_TABLE",
+              "field": ""
+            }
+          }
+        ],
+        "type": "TYPE_HALF_BANNER_THIN"
+      }
+    },
+    {
+      "rowType": "STRUCT_TYPE_LINE"
+    },
+    {
+      "rowType": "STRUCT_TYPE_ROW_HORIZONTAL_ITEM",
+      "rowData": {
+        "title": "اختصاصی سپهر",
+        "item": {
+          "data": null,
+          "type": "TYPE_VOD",
+          "onLoad": {
+            "method": "TYPE_SHOW_VODS",
+            "field": "66"
+          }
+        }
+      }
+    },
+    {
+      "rowType": "STRUCT_TYPE_BANNER",
+      "rowData": {
+        "title": "شبکه شیران",
+        "src": "https://sepehr.irib.ir/uploads/files/sepehrIRIB/android/common/images/v3/shiran2.jpg",
+        "onClick": {
+          "method": "OPEN_PAGE_CHANNEL",
+          "field": "15704"
+        }
+      }
+    },
+    {
+      "rowType": "STRUCT_TYPE_NEWLINE_8"
+    },
+    {
+      "rowType": "STRUCT_TYPE_ROW_HORIZONTAL_ITEM",
+      "rowData": {
+        "title": "همه کانال های رادیو نما",
+        "item": {
+          "data": null,
+          "type": "TYPE_LIVE_TV_GRID",
+          "onLoad": {
+            "method": "TYPE_SHOW_CHANNELS",
+            "field": "1077"
+          }
+        }
+      }
+    },
+    {
+      "rowType": "STRUCT_TYPE_ROW_HORIZONTAL_ITEM",
+      "rowData": {
+        "title": "فیلم سینمایی",
+        "item": {
+          "data": null,
+          "type": "TYPE_VOD",
+          "onLoad": {
+            "method": "TYPE_SHOW_VODS",
+            "field": "67"
+          }
+        }
+      }
+    },
+    {
+      "rowType": "STRUCT_TYPE_ROW_HORIZONTAL_ITEM",
+      "rowData": {
+        "title": "کودک و نوجوان",
+        "item": {
+          "data": null,
+          "type": "TYPE_VOD",
+          "onLoad": {
+            "method": "TYPE_SHOW_VODS",
+            "field": "351"
+          }
+        }
+      }
+    },
+    {
+      "rowType": "STRUCT_TYPE_LINE"
+    }
+  ]
+}
